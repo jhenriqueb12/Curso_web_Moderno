c7525a03e28bab155c9785308ce8349a9b509239
jhenriqueb12 <henrique.barros.hb@outlook.com>
2022-05-02 11:37:27 -0300

    Aulas de função

---
+++ 1.JavaScript/3.Funcao/3-This-pode-variar.mk
@@ -0,0 +1,7 @@
+*This Pode Variar*
+
+-Se for definida em escopo global, a palavra reservada "this" será estritamente igual (===) à palavra reservada "window"
+
+-Se for definida em escopo local (ex. functions) ele irá Variar
+
+-Ser for defiinida em escopo local, mas com "arrow function" ela será estritamente igual (===) à palavra reservada "window"
